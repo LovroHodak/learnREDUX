aff49e63a2bdf4549b124fb0805ace871fc6fbcb
lovro <hodak.lovro@gmail.com>
2021-02-17 15:44:53 +0100

    in seperated files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment} from './actions/index'
+import {decrement} from './actions/index'
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))} >+</button>
+      <button onClick={() => dispatch(decrement())} >-</button>
+
+      {isLogged ? <h3>Valuable info I shouldnt see</h3> : ''}
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,35 +4,11 @@ import "./index.css";
 import App from "./App";
 
 import {createStore} from 'redux'
+import allReducer from './reducers/index'
+
+import {Provider} from 'react-redux'
+
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 
-// 2. ACTION - increment (it is just a posibility it is not executed (it could also be decrement, multiply...))
-// ACTION is a function that return an Object
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-// 3. REDUCER - will check Action to modify Store (takes 2 params - state, action)
-const counter = (state = 0, action) => {
-  switch(action.type){
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-// 1. STORE => global state
-let store = createStore(counter)
-//display it in the console
-store.subscribe(() => console.log(store.getState()))
-// 4. DISPATCH - execute Action (dispatch that Action to Reducer)
-store.dispatch(increment())
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
