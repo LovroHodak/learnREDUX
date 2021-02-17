3b20035ef6519e7bf52996c1a333b08e6023a182
lovro <hodak.lovro@gmail.com>
2021-02-17 15:08:05 +0100

    everything done in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {createStore} from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// 2. ACTION - increment (it is just a posibility it is not executed (it could also be decrement, multiply...))
+// ACTION is a function that return an Object
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+// 3. REDUCER - will check Action to modify Store (takes 2 params - state, action)
+const counter = (state = 0, action) => {
+  switch(action.type){
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+  }
+}
+// 1. STORE => global state
+let store = createStore(counter)
+//display it in the console
+store.subscribe(() => console.log(store.getState()))
+// 4. DISPATCH - execute Action (dispatch that Action to Reducer)
+store.dispatch(increment())
+
+ReactDOM.render(<App />, document.getElementById("root"));
